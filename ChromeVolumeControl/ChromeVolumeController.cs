1ef9e48486cf8e1858692d056a00ccd1a2e47593
talarari <talarari@gmail.com>
2015-03-14 20:39:57 +0200

    initial commit

---
+++ ChromeVolumeControl/ChromeVolumeController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSCore.CoreAudioAPI;
+
+namespace ChromeVolumeControl
+{
+    public class ChromeVolumeController
+    {
+         private readonly Predicate<Process> _predicate;
+
+         public ChromeVolumeController(Predicate<Process> predicate)
+        {
+            _predicate = predicate;
+        }
+
+        public void SetVolume(int volume)
+        {
+            var volumes = Observable.Create<SimpleAudioVolume>(observer =>
+            {
+                var timer = new System.Timers.Timer { Interval = 100 };
+                timer.Elapsed += (sender, args) =>
+                {
+                    var audioVolumes = GetDefaultAudioSessionManager2(DataFlow.Render)
+                       .GetSessionEnumerator()
+                       .Select(session => session.QueryInterface<AudioSessionControl2>())
+                       .Where(session2 => _predicate(session2.Process))
+                       .Select(relevantSession => relevantSession.QueryInterface<SimpleAudioVolume>())
+                       .ToList();
+
+                    if (audioVolumes.Count > 0)
+                    {
+                        audioVolumes.ForEach(observer.OnNext);
+                        observer.OnCompleted();
+
+                        if (timer != null)
+                        {
+                            timer.Stop();
+                            timer.Dispose();
+                        }
+
+                    }
+
+
+                };
+                timer.Start();
+                return Disposable.Empty;
+            });
+
+
+            volumes.Subscribe(audioVolume => audioVolume.MasterVolume = volume / 100f);
+
+        }
+
+        private AudioSessionManager2 GetDefaultAudioSessionManager2(DataFlow dataFlow)
+        {
+            using (var enumerator = new MMDeviceEnumerator())
+            {
+                using (var device = enumerator.GetDefaultAudioEndpoint(dataFlow, Role.Multimedia))
+                {
+                    var sessionManager = AudioSessionManager2.FromMMDevice(device);
+                    return sessionManager;
+                }
+            }
+        }
+
+    }
+}
---
+++ ChromeVolumeControl/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChromeVolumeControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("type chrome title substring to look for in chrome tabs:");
+            var titleSubstring = Console.ReadLine();
+            var chromeVolumeControl =
+                new ChromeVolumeController(process => process != null && process.MainWindowTitle.Contains(titleSubstring));
+
+            string line;
+            Console.WriteLine("type a volume number from 0-100 to change chrome volume");
+            while ((line = Console.ReadLine()) != "stop")
+            {
+                var volume = int.Parse(line.Trim());
+                chromeVolumeControl.SetVolume(volume);
+            }
+        }
+    }
+}
